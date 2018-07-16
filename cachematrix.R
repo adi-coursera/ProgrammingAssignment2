06bacba83ac6fd9716b5b38c46ef38c16b4cebdf
adi-coursera <adithya.gng@icloud.com>
2018-07-16 12:27:40 +0530

    Committing own cacheMatrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,52 @@
-## Put comments here that give an overall description of what your
-## functions do
+## makeCacheMatrix takes as input a matrix and returns a list of functions that
+## retrieve the matrix and its inverse and allow one to reset them.
+##    cacheSolve checks a "matrix" of the above type and calculates and sets its
+## inverse only if said inverse has not already been calculated.
 
-## Write a short comment describing this function
+
+## Contains 4 functions get, set, setInverse and getInverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      i <- NULL
+      set <- function(p) {
+            x <<- p
+            i <<- NULL
+      }
+      get <- function()
+            x
+      setInverse <- function(inverse)
+            i <<- inverse
+      getInverse <- function()
+            i
+      list(
+            set = set,
+            get = get,
+            setInverse = setInverse,
+            getInverse = getInverse
+      )
+      
+      
 }
 
 
-## Write a short comment describing this function
+## cacheSolve checks if a matrix is square, then checks if the mean hasn't
+## already been calculated and sets it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      ## Return a matrix that is the inverse of 'x'
+      p <- x$get()
+      
+      if(NROW(p) != NCOL(p)){
+            print("Matrix is not square")
+            x$setInverse(NULL)
+      }
+      else{
+            if(!is.null(x$getInverse())){
+                  print("Getting cached inverse")
+                  return(x$getInverse())
+            }
+            i <- solve(p)
+            x$setInverse(i)
+            i
+      }
 }
